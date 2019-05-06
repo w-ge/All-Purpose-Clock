e24785d340f13e8aceb9ace8ae7d04fa4b943d2c
Bill <bill.ge.108@gmail.com>
2019-05-06 18:12:29 -0400

    Added Menu and Fonts

---
+++ js-stdclock.js
@@ -0,0 +1,33 @@
+﻿function getTime() {
+    let today = new Date();
+    let time = "";
+    
+    if (today.getHours() % 12 === 0) {
+        time = "12:";
+    }
+    else {
+        time = "" + (today.getHours() % 12) + ":";
+    }
+    if (today.getMinutes() < 10) {
+        time += "0" + today.getMinutes();
+    }
+    else {
+        time += today.getMinutes();
+    }
+    if (today.getHours() >= 12){
+        time += " PM";
+    }
+    else {
+        time += " AM";
+    }
+
+    $(".clockface").text(time);
+}
+
+$(document).ready(() => {
+    getTime();
+    let repeat = setInterval(getTime, 1000);
+   /* $(".arrow").on("click", () => {
+        $(".menu").slideToggle(400, "linear");
+    })*/
+});
