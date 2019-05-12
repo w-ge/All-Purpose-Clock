45d5c63e8cfeca08497783c3647a085eacf1b8e2
Bill <bill.ge.108@gmail.com>
2019-05-11 21:27:57 -0400

    Building Timer

--- Javascript/js-timer.js
+++ Javascript/js-timer.js
@@ -1,5 +1,21 @@
 let sec = 0;
 let min = 0;
+let time = "";
+
+function updateDisplay() {
+    time = "";
+    if (min <= 9) {
+        time += "0";
+    }
+    time += min + ":";
+
+    if (sec <= 9) {
+        time += "0";
+    }
+
+    time += sec;
+    $(".timerface").text(time);
+}
 $("document").ready(() => {
 
     // Mouseover Effect for Buttons
@@ -9,8 +25,39 @@ $("document").ready(() => {
         $(event.currentTarget).removeClass("buttonOver");
     });
 
+    // Add Minutes 
+    $("#minTop").on("click", () => {
+        min = (min + 1) % 99;
+        updateDisplay();
+    });
+
+    // Add Seconds 
+    $("#secTop").on("click", () => {
+        sec = (sec + 1) % 60;
+        updateDisplay();
+    });
+
+    // Subtract Minutes 
+    $("#minBot").on("click", () => {
+        min = (min - 1) % 99;
+        if (min < 0) {
+            min = 99;
+        }
+        updateDisplay();
+    });
+
+    // Subtract Seconds 
+    $("#secBot").on("click", () => {
+        sec = (sec - 1) % 60;
+        if (sec < 0) {
+            sec = 59;
+        }
+        updateDisplay();
+    });
+
+
     // Toggle Menu
     $(".arrow").on("click", () => {
         $(".menu").slideToggle(500);
-    })
+    });
 });
